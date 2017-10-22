cdd1e2a0bfd17cb4f1b2ef0729bd4b6f8623da1e
SagYernar <sagyernar@gmail.com>
2017-10-23 02:07:46 +0600

    All Done!

---
+++ TextRedactor/TextRedactor/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TextRedactor
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string buffer;
+        private int selectionStart;
+        private int selectionLenght;
+        private string selectedText;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextTextBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            selectionStart = textTextBox.SelectionStart;
+            selectionLenght = textTextBox.SelectionLength;
+            selectedText = textTextBox.SelectedText;
+        }
+
+        private void CopyButton_Click(object sender, RoutedEventArgs e)
+        {
+            buffer = selectedText;
+        }
+
+        private void CutButton_Click(object sender, RoutedEventArgs e)
+        {
+            buffer = selectedText;
+            textTextBox.Text = textTextBox.Text.Remove(selectionStart, selectionLenght);
+            
+        }
+
+        private void PasteButton_Click(object sender, RoutedEventArgs e)
+        {
+            textTextBox.Text = textTextBox.Text.Insert(selectionStart, buffer);
+        }
+
+    }
+}
